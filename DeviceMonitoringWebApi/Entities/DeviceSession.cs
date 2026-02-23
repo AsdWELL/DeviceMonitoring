b9f5109290449c5617d05ea8469057ae71e2ec64
AsdWELL <dimagodov.23@mail.ru>
2026-02-23 22:46:40 +0300

    Добавьте файлы проекта.

---
+++ DeviceMonitoringWebApi/Entities/DeviceSession.cs
@@ -0,0 +1,15 @@
+﻿namespace DeviceMonitoringWebApi.Entities
+{
+    public class DeviceSession
+    {
+        public int Id { get; set; }
+
+        public string DeviceId { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Version { get; set; }
+    }
+}
---
+++ DeviceMonitoringWebApi/Repositories/Interfaces/IDeviceSessionRepository.cs
@@ -0,0 +1,13 @@
+﻿using DeviceMonitoringWebApi.Entities;
+
+namespace DeviceMonitoringWebApi.Repositories.Interfaces
+{
+    public interface IDeviceSessionRepository
+    {
+        Task<int> AddDeviceSession(DeviceSession deviceSession);
+
+        Task<List<DeviceSession>> GetAllDevices();
+
+        Task<List<DeviceSession>> GetDeviceSessionsByDeviceId(int deviceId);
+    }
+}
